b2fbce15a4cd2e749d9abdca6c4a696c62c106b1
Patrick Schless <patrick@plainlystated.com>
2012-02-11 18:45:02 -0600

    update section teaser photos

--- config/sections.rb
+++ config/sections.rb
@@ -1,12 +1,12 @@
 Sections = {
   :state_mirrors => Section.new(
     :title => "State Mirrors",
-    :image => "https://lh5.googleusercontent.com/-KaRlFq-9gPA/TrXG4-fU2vI/AAAAAAAAIYg/R8cNM7AvsKw/s300/square_wave_square.jpg",
+    :image => "http://img2.etsystatic.com/il_fullxfull.308226106.jpg",
     :description => "State mirrors are cut into the shape of your favorite state! Lots of states are available already, and custom ones are always available at no extra change. I can also do mirrors in the shape of your country."
   ),
   :android_mirrors => Section.new(
     :title => "Android Mirrors",
-    :image => "https://lh5.googleusercontent.com/-KaRlFq-9gPA/TrXG4-fU2vI/AAAAAAAAIYg/R8cNM7AvsKw/s300/square_wave_square.jpg",
+    :image => "http://img2.etsystatic.com/il_fullxfull.306577922.jpg",
     :description => "Make your iPhone&trade; friends jealous with these Android&trade; mirrors!"
   ),
   :artistic_mirrors => Section.new(
